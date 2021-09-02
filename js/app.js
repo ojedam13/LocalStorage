2f6c50ae0469fc638d5b9c20ad07ef150f084a89
ojedam13 <m_ojeda17@hotmail.com>
2021-09-02 18:28:21 -0300

    validando formulario

--- js/app.js
+++ js/app.js
@@ -15,5 +15,28 @@ function eventListeners() {
 function agregarTweet(e) {
     e.preventDefault();
 
-    console.log('agregando twet')
+    //textarea donde el usuario escribe 
+    const tweet = document.querySelector('#tweet').value;
+
+    //validación..
+    if (tweet === '') {
+        mostrarError('Un mensaje no puede ir vacio')
+
+        return; //evita q se ejecuten más lineas de codigo
+    }
+}
+
+//mostrar msj de error
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+    //elimina la alerta desp de 3s
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
